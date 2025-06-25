2cf91f258c614958427f19dd83445886095c3297
danielacosta21 <127493701+danielacosta21@users.noreply.github.com>
2025-06-25 23:43:23 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.members = True
 intents.guilds = True
+intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
