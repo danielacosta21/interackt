aea6b838c34dd21e4f00056addf70418ce174f21
danielacosta21 <127493701+danielacosta21@users.noreply.github.com>
2025-06-26 00:31:23 +0800

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,30 @@
+import os
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.members = True
+intents.guilds = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+NEW_ROLES = ["FST", "Circuiteer"]
+OLD_ROLE = "Wanderer"
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+@bot.event
+async def on_member_update(before: discord.Member, after: discord.Member):
+    added_roles = set(after.roles) - set(before.roles)
+    for role in added_roles:
+        if role.name in NEW_ROLES:
+            old_role = discord.utils.get(after.guild.roles, name=OLD_ROLE)
+            if old_role in after.roles:
+                await after.remove_roles(old_role)
+                print(f"Removed {OLD_ROLE} from {after.display_name} due to gaining {role.name}")
+            break
+
+bot.run(os.getenv("DISCORD_TOKEN"))
